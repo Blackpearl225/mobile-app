7e4a9c4474c649523210eace8c2c217c854159db
Vaalrhona <lucille.lvt@gmail.com>
2021-05-20 09:45:47 +0200

    right update

--- index.js
+++ index.js
@@ -1,10 +1,64 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+require('dotenv').config()
+
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.set('port', (process.env.PORT || 5000));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/public');
+app.get('/cool', (req, res) => res.send(cool()))
+app.get('/times', (req, res) => res.send(showTimes()))
+.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT Name, Email FROM salesforce.contact');
+    const results = { 'results': (result) ? result.rows : null};
+    var test = result.rows;
+    var stringToParse = JSON.stringify(test);
+    
+    res.render('view.html', { names: stringToParse} );
+    console.log(test[0].name);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+
+app.get('/', function(request, response) {
+  var env = process.env.APP_ENV;
+  if (env == 'staging') {
+    var envName = 'staging'
+  } else if (env == 'production') {
+    var envName = 'production'
+  } else {
+    var envName = 'review app'
+  }
+  response.render('index.html', { env: envName});
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app running at localhost:" + app.get('port'));
+});
+
+module.exports = app
+
+showTimes = () => {
+  let result = '';
+  const times = process.env.TIMES || 5;
+  for(i = 0; i < times; i++){
+    result += i + ' ';
+  }
+  return result;
+}
